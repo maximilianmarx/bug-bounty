2e16f13284757ef1b0bc72b5f62ee3524be225e2
Max <49280556+maximilianmarx@users.noreply.github.com>
2022-08-25 17:07:36 +0200

    Refactor

--- commonpeak2.py
+++ commonpeak2.py
@@ -3,16 +3,24 @@
 # Generates a list of subdomains (~485k) using the commonspeak2 wordlist
 # https://github.com/assetnote/commonspeak2-wordlists/blob/master/subdomains/subdomains.txt
 
-domain = 'example.com'
+import os
+import sys
 
+if(len(sys.argv) < 2):
+        sys.exit("[*] Usage: commonspeak.py <domain>")
+
+domain = str(sys.argv[1])
 wordlist = open('/usr/share/wordlists/commonspeak2.txt').read().split('\n')
-f = open("commonspeak2.out", "a")
+
+out = os.path.join(os.getcwd(),"commonspeak2.out")
+f = open(out, "a")
 
 for word in wordlist:
-	if not word.strip():
-		continue
-	s = '{}.{}'.format(word.strip(), domain)
-	f.write((s + "\n"))
+        if not word.strip():
+                continue
+        s = '{}.{}'.format(word.strip(), domain)
+        f.write((s + "\n"))
 
-wordlist.close()
 f.close()
+
+print("[*] File written in " + str(out))
